9b6215e7e45dc64b4c4a4b935301d5bfc2831b2c
Melissa Bruno <brunomr@miamioh.edu>
2017-05-09 20:20:56 -0400

    Add files via upload

---
+++ VelocityOfBall.ino
@@ -0,0 +1,43 @@
+/* Measures the speed of a ping pong ball using the distance
+   between two photoresistors and millis() */
+
+int sensor1Value;
+int sensor2Value;
+double startTime;
+double endTime;
+double time;
+double timeInSeconds;
+double distance;
+double speed;
+boolean endFlag;
+
+void setup() {
+  Serial.begin(9600);
+  distance = 6;
+  endFlag = false;
+}
+
+void loop() {
+  sensor1Value = analogRead(A0);
+  sensor2Value = analogRead(A1);
+
+  //Serial.println(sensor1Value);       // Used for determining photoresistor thresholds
+  //Serial.println(sensor2Value);
+
+  if(sensor1Value > 800){               // If first photoresistor passes threshold, save startTime.
+    endFlag = false;
+    startTime = millis();
+  }
+
+  if(sensor2Value < 830 && endFlag == false){       // If second photoresistor passes threshold, save endTime.
+   endTime = millis();
+    time = endTime - startTime;
+    timeInSeconds = time/1000;
+    speed = distance/timeInSeconds;
+    Serial.print("Speed: ");
+    Serial.print(speed);
+    Serial.print(" cm/s");
+    Serial.println(" ");
+    endFlag = true;
+  }
+}
